910ac76ea63a751b4dd3c981548c3a1089dd6e2e
Antonino Zichichi <antonino.zichichi@yahoo.it>
2022-10-03 16:41:30 +0200

    fine navbar mobile + desktop --  insert media query -- insert animation link navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,35 +1,61 @@
 import { useState } from "react"
+import style from "styles/shared/navbar.module.scss"
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
-
-    const hendelClick = () => {
- 
-    }
-
-    <div className="relative">
-      <div className="w-full h-[95px] px-[20px] lg:px-[40px] bg-white border-yellow-100	border-solid	border-b-2 flex justify-between items-center">
-        <a className="text-black-100">
-          <p className="font-londrina font-normal text-4xl">NOUNS COMICS</p>
-          <p className="font-ubuntu font-light text-xs">Powered by ComicsDAO</p>
-        </a>
-        <div className="relative w-[40px] h-[25px]" onClick={hendelClick}>
-          <span className="h-[2px] w-full bg-black-100 absolute rounded top-0"></span>
+  const hendelClick = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div className="relative w-full h-[95px] border-yellow-100	border-solid	border-b-2 px-[20px] md:px-[40px] md:flex justify-between items-center">
+      <div className="h-full bg-white flex justify-between items-center z-10 relative">
+        <p className="text-black-100 flex flex-col">
+          <span className="font-londrina font-normal text-4xl">
+            NOUNS COMICS
+          </span>
+          <span className="font-ubuntu font-light text-xs">
+            Powered by ComicsDAO
+          </span>
+        </p>
+        <div
+          className="relative w-[40px] h-[25px] md:hidden"
+          onClick={hendelClick}
+        >
+          <span
+            className={`h-[2px] w-full bg-black-100 absolute rounded transition-all	${
+              isOpen ? "top-[50%] opacity-0" : "top-0"
+            }`}
+          ></span>
           <span className="h-[2px] w-full bg-black-100 absolute rounded top-[50%]"></span>
-          <span className="h-[2px] w-full bg-black-100 absolute rounded bottom-0"></span>
+          <span
+            className={`h-[2px] w-full bg-black-100 absolute rounded transition-all	${
+              isOpen ? "bottom-[50%] opacity-0" : "bottom-0"
+            }`}
+          ></span>
         </div>
-        <ul className="absolute bg-black-100 w-full px-[20px] py-[40px] h-[95px] flex justify-between items-center left-0 -bottom-full">
-          <li>
-            <a>Support</a>
-          </li>
-          <li>
-            <a>Join Discord</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
-        </ul>
       </div>
+      <ul
+        className={`absolute w-full px-[20px] h-[95px] flex justify-between items-center left-0 transition-all 
+        ${
+          isOpen ? "opacity-100 -bottom-full" : "opacity-0 bottom-0"
+        } md:static md:opacity-100 md:px-0 md:h-full md:w-1/2	`}
+      >
+        <li
+          className={`font-ubuntu font-normal text-base relative py-[3px] px-1 cursor-pointer ${style.link_after}`}
+        >
+          <a>Support</a>
+        </li>
+        <li
+          className={`font-ubuntu font-normal text-base relative py-[3px] px-1 cursor-pointer ${style.link_after}`}
+        >
+          <a>Join Discord</a>
+        </li>
+        <li
+          className={`font-ubuntu font-normal text-base relative py-[3px] px-1 cursor-pointer ${style.link_after}`}
+        >
+          <a>Connect</a>
+        </li>
+      </ul>
     </div>
   )
 }
