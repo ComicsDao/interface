7871d0cd83768d0f9355c09b6d68f21255a1dddc
Antonino Zichichi <antonino.zichichi@yahoo.it>
2022-10-03 18:34:52 +0200

    fixed bug navbar position

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -7,8 +7,8 @@ const Navbar = () => {
     setIsOpen(!isOpen)
   }
   return (
-    <div className="relative w-full h-[95px] border-yellow-100	border-solid	border-b-2 px-[20px] md:px-[40px] md:flex justify-between items-center">
-      <div className="h-full bg-white flex justify-between items-center z-10 relative">
+    <div className="max-w-screen-2xl fixed w-full h-[95px] border-yellow-100	border-solid	border-b-2 md:px-[40px] md:flex justify-between items-center bg-white">
+      <div className="h-full bg-white flex justify-between items-center z-10 relative px-[20px] md:px-0">
         <p className="text-black-100 flex flex-col">
           <span className="font-londrina font-normal text-4xl">
             NOUNS COMICS
@@ -35,23 +35,23 @@ const Navbar = () => {
         </div>
       </div>
       <ul
-        className={`absolute w-full px-[20px] h-[95px] flex justify-between items-center left-0 transition-all 
+        className={`absolute w-full px-[20px] h-[45px] pl-[40%] flex items-center justify-between left-0 transition-all	 bg-yellow-100 text-black-100
         ${
-          isOpen ? "opacity-100 -bottom-full" : "opacity-0 bottom-0"
-        } md:static md:opacity-100 md:px-0 md:h-full md:w-1/2	`}
+          isOpen ? "opacity-100 -bottom-[45px]" : "opacity-0 bottom-0"
+        } md:static md:opacity-100 md:px-0 md:h-full md:w-1/2 md:bg-transparent `}
       >
         <li
-          className={`font-ubuntu font-normal text-base relative py-[3px] px-1 cursor-pointer ${style.link_after}`}
+          className={`font-ubuntu font-normal text-xs relative py-[3px] px-1 cursor-pointer md:text-base ${style.link_after}`}
         >
           <a>Support</a>
         </li>
         <li
-          className={`font-ubuntu font-normal text-base relative py-[3px] px-1 cursor-pointer ${style.link_after}`}
+          className={`font-ubuntu font-normal text-xs relative py-[3px] px-1 cursor-pointer md:text-base ${style.link_after}`}
         >
           <a>Join Discord</a>
         </li>
         <li
-          className={`font-ubuntu font-normal text-base relative py-[3px] px-1 cursor-pointer ${style.link_after}`}
+          className={`font-ubuntu font-normal text-xs relative py-[3px] px-1 cursor-pointer md:text-base ${style.link_after}`}
         >
           <a>Connect</a>
         </li>
