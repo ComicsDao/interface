7bc825fa0005dd93effbe75d5b5d79e0ed1793fd
Dom-Mac <domenicochad@gmail.com>
2022-10-03 13:34:01 +0200

    nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import "../styles/globals.css"
+import "../styles/globals.scss"
 import type { AppProps } from "next/app"
+import { Navbar } from "../components/shared"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="m-auto max-w-screen-2xl">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
