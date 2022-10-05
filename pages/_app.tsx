28e581fdde9b5e37954a28e4d92249eb1a5c3f17
Antonino Zichichi <antonino.zichichi@yahoo.it>
2022-10-05 12:59:15 +0200

    simple home finish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "../styles/globals.scss"
 import type { AppProps } from "next/app"
-import { Navbar } from "../components/shared"
 import { ScreenSizeProvider } from "../provider/ScreenSizeProvider"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="m-auto max-w-screen-2xl">
-      <Navbar />
       <ScreenSizeProvider>
         <Component {...pageProps} />
       </ScreenSizeProvider>
