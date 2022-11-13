2820bbf795354c4e8341207b68426c608f4d244c
Dom-Mac <domenicochad@gmail.com>
2022-11-13 19:59:00 +0100

    fix alchemy provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,10 @@ import { publicProvider } from "wagmi/providers/public"
 
 const { chains, provider } = configureChains(
   [chain.mainnet],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+    publicProvider()
+  ]
 )
 
 const { connectors } = getDefaultWallets({
